2c6af6e68ad3123c3143a158e86da03828c55671
Starklord17 <maxi.sm117@gmail.com>
2024-06-28 23:18:38 -0300

    :splarkles: Menu lateral

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { TopMenu } from "@/components";
+import { Sidebar, TopMenu } from "@/components";
 
 // lrc => Create a layout root component
 export default function ShopLayout({
@@ -9,6 +9,7 @@ export default function ShopLayout({
   return (
     <main className="min-h-screen">
       <TopMenu />
+      <Sidebar />
 
       <div className="px-0 sm:px-10">
         {children}
