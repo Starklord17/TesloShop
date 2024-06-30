99f69deb1839f3726ab709b7d04d69f88b114ad5
Starklord17 <maxi.sm117@gmail.com>
2024-06-30 09:48:00 -0300

    Category Page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,49 @@
 import { notFound } from "next/navigation";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import { ValidCategory } from '@/interfaces';
+
+const seedProducts = initialData.products;
 
 interface Props {
   params: {
-    id: string;
+    id: ValidCategory;
   }
 }
 
 export default function CategoryPage({params}: Props) {
 
   const {id} = params;
+  const products = seedProducts.filter(product => product.gender === id);
 
-  if (id === 'kids') {
+  // const labels: {[key: ValidCategories]: string} = {
+  const labels: Record<ValidCategory, string> = {
+    'men': 'para hombres',
+    'women': 'para mujeres',
+    'kid': 'para niños',
+    'unisex': 'para todos'
+  }
+
+  // if (id !== 'kids') {
+  //   notFound();
+  // }
+
+  if (!products || !products.length || !labels[id as ValidCategory]) {
     notFound();
   }
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title 
+        title={`Artículos ${(labels)[id]}`}
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+
+      <ProductGrid 
+        products={products}
+      />
+
+    </>
   );
 }
