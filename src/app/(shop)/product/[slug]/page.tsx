4413790ce9fe2a3a8af3419c3fff311f0064d259
Starklord17 <maxi.sm117@gmail.com>
2024-06-30 11:24:34 -0300

    Pantalla del producto

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,49 @@
+import { notFound } from "next/navigation";
+import { initialData } from "@/seed/seed";
+import { titleFont } from "@/config/fonts";
 
-export default function ProductPage() {
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ProductPage({ params }: Props) {
+  const { slug } = params;
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
-    </div>
+    <main className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+
+      {/* SlideShow */}
+      <section className="col-span-1 md:col-span-2">
+        <h1>SlideShow</h1>
+      </section>
+
+      {/* Details */}
+      <section className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+
+        {/* Selector de Tallas */}
+
+        {/* Selector de cantidad */}
+
+        {/* Button */}
+        <button className="btn-primary my-5">Add to Cart</button>
+
+        {/* Description */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p>{product.description}</p>
+      </section>
+
+    </main>
   );
 }
