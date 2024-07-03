871faa34eb7f952ac8d160b520c03c2e112416fa
MaxSM <maxi.sm117@gmail.com>
2024-07-03 15:48:00 -0300

    Pantalla del cart

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -7,65 +7,76 @@ const productsInCart = [
   initialData.products[0],
   initialData.products[1],
   initialData.products[2],
-]
+];
 
 export default function CartPage() {
   return (
     <main className="flex justify-center items-center mb-72 px-10 sm:px-0">
-
       <section className="flex flex-col w-[1000px]">
-
-        <Title 
-          title="Cart" 
-          subtitle="Your cart is empty"
-        />
+        <Title title="Cart" subtitle="Your cart is empty" />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Carrito */}
+          <article className="flex flex-col mt-5">
+            <h2 className="text-xl">Agregar más items</h2>
+            <Link href="/" className="underline mb-5">
+              Continúa comprando
+            </Link>
 
-        {/* Carrito */}
-        <div className="flex flex-col mt-5">
-          <span className="text-xl">Agregar más items</span>
-          <Link href="/" className="underline mb-5">
-            Continúa comprando
-          </Link>
-        </div>
+            {/* Items */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  style={{
+                    objectFit: "cover",
+                  }}
+                  className="mr-5 rounded"
+                />
 
-        {/* Items */}
-        {
-          productsInCart.map( product => (
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={3} />
 
-            <div key={product.slug} className="flex">
-              <Image
-                src={`/products/${product.images[0]}`}
-                alt={product.title}
-                width={100}
-                height={100}
-                className="mr-5 rounded"
-              />
-
-              <div>
-                <p>{product.title}</p>
-                <p>${product.price}</p>
-                <QuantitySelector quantity={3} />
-
-                <button className="underline mt-3">
-                  Remover
-                </button>
-
+                  <button className="underline mt-3">Remover</button>
+                </div>
               </div>
+            ))}
+          </article>
 
+          {/* Checkout - Resumen de orden*/}
+          <aside className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-xl mb-2">Resumen de orden</h2>
+
+            <div className="grid grid-cols-2 gap-2">
+              <span>Nº Productos</span>
+              <span className="text-right">3 artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 15</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-2xl text-right">$ 115</span>
             </div>
 
-          ))
-        }
-
-        {/* Checkout */}
-
-
+            <div className="mt-5 mb-2 w-full">
+              <Link
+                className="flex btn-primary justify-center"
+                href="/checkout/address"
+              >
+                Checkout
+              </Link>
+            </div>
+          </aside>
         </div>
-
       </section>
-
     </main>
   );
 }
