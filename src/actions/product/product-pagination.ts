a5826e929bf434c3b5f81ac860faa9a4ca7a9008
Starklord17 <maxi.sm117@gmail.com>
2024-07-07 11:14:38 -0300

    Paginación - Determinar el total de páginas

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -17,6 +17,7 @@ export const getPaginatedProductsWithImages = async({
 
   try {
 
+    // 1. Obtener los productos
     const products = await prisma.product.findMany({
       take: take,
       skip: (page - 1) * take,
@@ -28,11 +29,18 @@ export const getPaginatedProductsWithImages = async({
           }
         }
       }
-    })
+    });
 
     // console.log(products);
+
+    // 2, Obtener el total de páginas
+    // TODO; 
+    const totalCount = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCount / take);
     
     return {
+      currentPage: page,
+      totalPages: totalPages,
       products: products.map(product => ({
         ...product,
         images: product.ProductImage.map(image => image.url)
