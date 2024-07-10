8d45251f5c08fb18550e0f49d2fda94c0c5fdf54
Starklord17 <maxi.sm117@gmail.com>
2024-07-09 23:34:07 -0300

    :sparkles: Skeleton - Mostrar loading en el stock

--- src/components/product/stock-label/StockLabel.tsx
+++ src/components/product/stock-label/StockLabel.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 import { getStockBySlug } from "@/actions";
@@ -7,23 +7,30 @@ interface Props {
   slug: string;
 }
 
-export const StockLabel = ({slug}:Props) => {
-
+export const StockLabel = ({ slug }: Props) => {
   const [stock, setStock] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getStock();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getStock = async () => {
     const inStock = await getStockBySlug(slug);
     setStock(inStock);
-  }
+    setIsLoading(false);
+  };
 
   return (
-    <p className={`antialiased text-l`}>
-      Stock: {stock}
-    </p>
+    <>
+      {
+        isLoading ? (
+          <p className={`antialiased text-l bg-gray-200 animate-pulse w-20 rounded-md`}>&nbsp;</p>
+        ) : (
+          <p className={`antialiased text-l`}>Stock: {stock}</p>
+        )
+      }
+    </>
   );
 };
