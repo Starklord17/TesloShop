cf28b474c62e101416ef78f886698546b91b9c6c
Starklord17 <maxi.sm117@gmail.com>
2024-07-12 12:05:53 -0300

    Configuración de Credentials Provider

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,6 @@
-import type { NextAuthConfig } from 'next-auth';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
  
 export const authConfig: NextAuthConfig = {
   pages: {
@@ -6,6 +8,30 @@ export const authConfig: NextAuthConfig = {
     newUser: '/auth/new-account',
   },
   providers: [
-    
+    credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+          if (!parsedCredentials.success) {
+            return null;
+          }
+
+          const { email, password } = parsedCredentials.data;
+
+          console.log({email,password});
+
+          // Buscar el correo
+
+          // Comparar las contraseñas
+
+          // Retornar el usuario
+
+          return null;
+      },
+    }),
   ],
 };
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
