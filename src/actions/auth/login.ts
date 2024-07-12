134adaea326ffbb0c2f97d8fc0999f31ff30cfa5
Starklord17 <maxi.sm117@gmail.com>
2024-07-12 20:52:16 -0300

    Mejorar la experiencia de usuario

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,24 +1,27 @@
-'use server';
- 
-import { signIn } from '@/auth.config';
-import { AuthError } from 'next-auth';
- 
+"use server";
+
+import { signIn } from "@/auth.config";
+import { sleep } from "@/utils";
+import { AuthError } from "next-auth";
+
 // ...
- 
+
 export async function authenticate(
   prevState: string | undefined,
-  formData: FormData,
+  formData: FormData
 ) {
   try {
+    // await sleep(2);
+
     console.log(Object.fromEntries(formData));
-    await signIn('credentials', formData);
+    await signIn("credentials", formData);
   } catch (error) {
     // if (error instanceof AuthError) {
     //   switch (error.type) {
-    //     case 'CredentialsSignin':
-    //       return 'Invalid credentials.';
-    //     default: 
-    //       return 'Something went wrong.';
+    //     case "CredentialsSignin":
+    //       return "Invalid credentials.";
+    //     default:
+    //       return "Something went wrong.";
     //   }
     // }
     // throw error;
