e9dc125a156b86d2d595e8062c840c8ec7e4a60a
Starklord17 <maxi.sm117@gmail.com>
2024-07-16 23:32:30 -0300

    Seed Countries

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,22 +1,39 @@
 import prisma from "../lib/prisma";
 import { initialData } from "./seed";
+import { countries } from "./seed-countries";
 
 async function main() {
   // console.log(initialData);
 
   // await Promise.all([
-    // 1. Borrar registros previos
-    // prisma.productImage.deleteMany(),
+  // 1. Borrar registros previos
+  // prisma.productImage.deleteMany(),
   //   prisma.product.deleteMany(),
   //   prisma.category.deleteMany(),
   // ]);
 
   await prisma.user.deleteMany();
+  await prisma.country.deleteMany();
+
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
 
   // console.log("Datos eliminados");
+
+  // Intregración de seed-countries.ts
+  // Países
+  // const countriesData = countries.map((country) => ({
+  //   name: country.name,
+  //   id: country.id,
+  // }));
+  // await prisma.country.createMany({
+  //   data: countriesData,
+  // });
+
+  await prisma.country.createMany({
+    data: countries,
+  })
 
   // Categorias
   // await prisma.category.create({
@@ -31,21 +48,27 @@ async function main() {
 
   const { categories, products, users } = initialData;
 
+  // Inserta todos los usuarios de initialData en la base de datos de una sola vez.
   await prisma.user.createMany({
     data: users,
   });
-  
+
+  // Transforma el array categories (que contiene solo nombres) en un array de objetos
+  // con la estructura { name: category }, que es el formato que espera Prisma.
   const categoriesData = categories.map((category) => ({
-    name: category
+    name: category,
   }));
   // console.log(categoriesData);
+  // Inserta todas las categorías transformadas en la base de datos.
   await prisma.category.createMany({
     data: categoriesData,
   });
 
-  const categoriesDB = await prisma.category.findMany(); // Se obtienen las categorias de la base de datos
+  // Se obtienen las categorias de la base de datos
+  const categoriesDB = await prisma.category.findMany();
   // console.log(categoriesDB);
 
+  // Transforma el array categoriesDB en un objeto con la estructura { category: id },
   const categoriesMap = categoriesDB.reduce((map, category) => {
     map[category.name.toLowerCase()] = category.id;
     return map;
@@ -64,33 +87,35 @@ async function main() {
   //   }
   // })
 
-  products.forEach( async(product) => {
-    
+  products.forEach(async (product) => {
+    // Desestructura el producto para obtener el tipo (que se usará como clave en categoriesMap), las imágenes y el resto de los datos del producto.
     const { type, images, ...rest } = product;
 
+    // Inserta el producto en la base de datos, utilizando el categoryId obtenido del mapa categoriesMap para asociarlo con la categoría correcta.
     const dbProduct = await prisma.product.create({
       data: {
         ...rest,
-        categoryId: categoriesMap[type]
-      }
+        categoryId: categoriesMap[type],
+      },
     });
 
     // Images
+    // Se crea un array imagesData con la estructura que espera Prisma para las imágenes de producto.
     const imagesData = images.map((image) => ({
       url: image,
-      productId: dbProduct.id
+      productId: dbProduct.id,
     }));
 
+    // Inserta las imágenes del producto en la base de datos.
     await prisma.productImage.createMany({
-      data: imagesData
+      data: imagesData,
     });
-
   });
 
-  console.log('Seed ejecutado correctamente');
-  
+  console.log("Seed ejecutado correctamente");
 }
 
+// Ejecuta la función main si el entorno de ejecución no es producción.
 (() => {
   if (process.env.NODE_ENV === "production") return;
 
