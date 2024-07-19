7a47a59be2caf2d22d1c2cc2c190f814b9e12788
Starklord17 <maxi.sm117@gmail.com>
2024-07-19 17:58:58 -0300

    Colocar orden - Productos y cantidades

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,8 @@
 
 import { auth } from "@/auth.config";
 import type { Address, ValidSize } from "@/interfaces";
+import prisma from '@/lib/prisma';
+
 
 interface ProductToOrder {
   productId: string;
@@ -14,6 +16,7 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
   const session = await auth();
   const userId = session?.user.id;
 
+  // Verificar sesión de usuario
   if (!userId) {
     return {
       ok: false,
@@ -21,6 +24,24 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
     }
   }
 
-  console.log({productIds, address, userId});
+  // console.log({productIds, address, userId});
+
+  // Obtener la info de los productos. Nota: Se pueden llevar 2 o más productos con el mismo ID
+
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map(p => p.productId)
+      }
+    }
+  });
+
+  // console.log(products);
+
+  // Calcular los montos // Encabezado
+  const itemsInOrder = productIds.reduce((acc, product) => acc + product.quantity, 0);
+  // console.log({itemsInOrder});
+
+  // Los totales de tax, subtotal, y total
 
 }
