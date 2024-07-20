f96f24b309bb1afb054e623da7599bc349afc684
Starklord17 <maxi.sm117@gmail.com>
2024-07-19 23:56:28 -0300

    Transacción - Actualizar inventario

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -1,9 +1,8 @@
-'use server';
+"use server";
 
 import { auth } from "@/auth.config";
 import type { Address, ValidSize } from "@/interfaces";
-import prisma from '@/lib/prisma';
-
+import prisma from "@/lib/prisma";
 
 interface ProductToOrder {
   productId: string;
@@ -11,8 +10,10 @@ interface ProductToOrder {
   size: ValidSize;
 }
 
-export const placeOrder = async (productIds: ProductToOrder[], address: Address) => {
-
+export const placeOrder = async (
+  productIds: ProductToOrder[],
+  address: Address
+) => {
   const session = await auth();
   const userId = session?.user.id;
 
@@ -20,8 +21,8 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
   if (!userId) {
     return {
       ok: false,
-      message: 'User not authenticated'
-    }
+      message: "User not authenticated",
+    };
   }
 
   // console.log({productIds, address, userId});
@@ -31,84 +32,134 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
   const products = await prisma.product.findMany({
     where: {
       id: {
-        in: productIds.map(p => p.productId)
-      }
-    }
+        in: productIds.map((p) => p.productId),
+      },
+    },
   });
 
   // console.log(products);
 
   // Calcular los montos // Encabezado
-  const itemsInOrder = productIds.reduce((acc, product) => acc + product.quantity, 0);
-  // Para cada producto en el arreglo productIds, se suma la quantity del producto actual 
-  // al valor acumulado hasta ese momento. El resultado final de reduce es el total de todas 
+  const itemsInOrder = productIds.reduce(
+    (acc, product) => acc + product.quantity,
+    0
+  );
+  // Para cada producto en el arreglo productIds, se suma la quantity del producto actual
+  // al valor acumulado hasta ese momento. El resultado final de reduce es el total de todas
   // las cantidades de los productos en la orden, que se almacena en la variable itemsInOrder.
 
   // Los totales de tax, subtotal, y total
-  const { subTotal, tax, total } = productIds.reduce( (totals, item ) => {
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+      const product = products.find((product) => product.id === item.productId);
 
-    const productQuantity = item.quantity;
-    const product = products.find(product => product.id === item.productId);
+      if (!product) throw new Error(`${item.productId} not found - 500`);
 
-    if (!product) throw new Error(`${item.productId} not found - 500`);
+      const subTotal = product.price * productQuantity;
 
-    const subTotal = product.price * productQuantity;
+      totals.subTotal += subTotal;
+      totals.tax += subTotal * 0.15;
+      // totals.total += subTotal + totals.tax;
+      totals.total += subTotal * 1.15;
 
-    totals.subTotal += subTotal;
-    totals.tax += subTotal * 0.15;
-    // totals.total += subTotal + totals.tax;
-    totals.total += subTotal * 1.15;
+      return totals;
 
-    return totals;
+      // return { subTotal: 0, tax: 0, total: 0 };
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+  );
 
-    // return { subTotal: 0, tax: 0, total: 0 };
-  }, { subTotal: 0, tax: 0, total: 0 });
-
-  // console.log({subTotal, tax, total}); 
+  // console.log({subTotal, tax, total});
 
   // Crear la transacción de base de datos
-  const prismaTx = await prisma.$transaction( async(tx) => {
-    // 1. Actualizar el stock de los productos
+  try {
 
-    // 2. Crear la orden - Encabezado - Detalle
-    const order = await tx.order.create({
-      data: {
-        userId: userId,
-        itemsInOrder: itemsInOrder,
-        subTotal: subTotal,
-        tax: tax,
-        total: total,
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      // 1. Actualizar el stock de los productos
+      const updatedProductsPromises = products.map((product) => {
+        // Acumular los valores
+        const productQuantity = productIds
+          .filter((p) => p.productId === product.id)
+          .reduce((acc, item) => item.quantity + acc, 0);
 
-        OrderItem: {
-          createMany: {
-            data: productIds.map(p => ({
-              quantity: p.quantity,
-              size: p.size,
-              productId: p.productId,
-              price: products.find(product => product.id === p.productId)?.price ?? 0
-            }))
-          }
+        if (productQuantity === 0) {
+          throw new Error(`Producto ${product.id} no tiene cantidad definida`);
         }
-      }
-    })
 
-    // Validar, si el price es cero, entonces, lanzar un error
+        return tx.product.update({
+          where: { id: product.id },
+          data: {
+            // inStock: product.inStock - productQuantity // No hacer
+            inStock: {
+              decrement: productQuantity,
+            },
+          },
+        });
+      });
 
-    // 3. Crear la dirección de la orden
-    const {country: countryId, ...restAddress} = address;
-    const orderAddress = await tx.orderAddress.create({
-      data: {
-        ...restAddress,
-        countryId,
-        orderId: order.id
-      }
-    })
+      const updatedProducts = await Promise.all(updatedProductsPromises);
+      // Verificar valores negativos en las existencias = no hay stock
+      updatedProducts.forEach((product) => {
+        if (product.inStock < 0) {
+          throw new Error(`${product.title} no tiene stock suficiente`);
+        }
+      });
+
+      // 2. Crear la orden - Encabezado - Detalle
+      const order = await tx.order.create({
+        data: {
+          userId: userId,
+          itemsInOrder: itemsInOrder,
+          subTotal: subTotal,
+          tax: tax,
+          total: total,
+
+          OrderItem: {
+            createMany: {
+              data: productIds.map((p) => ({
+                quantity: p.quantity,
+                size: p.size,
+                productId: p.productId,
+                price:
+                  products.find((product) => product.id === p.productId)
+                    ?.price ?? 0,
+              })),
+            },
+          },
+        },
+      });
+
+      // Validar, si el price es cero, entonces, lanzar un error
+
+      // 3. Crear la dirección de la orden
+      const { country: countryId, ...restAddress } = address;
+      const orderAddress = await tx.orderAddress.create({
+        data: {
+          ...restAddress,
+          countryId,
+          orderId: order.id,
+        },
+      });
+
+      return {
+        updatedProducts: updatedProducts,
+        order: order,
+        orderAddress: orderAddress,
+      };
+    });
 
     return {
-      updatedProducts: [],
-      order: order,
-      orderAddress: orderAddress,
-    }
-  })
+      ok: true,
+      order: prismaTx.order,
+      prismaTx: prismaTx,
+    };
 
-}
+  } catch (error: any) {
+    console.error(error);
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
