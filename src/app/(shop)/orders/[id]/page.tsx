8fdc914bde6621deb9e5a2cd4b33a81204f7c7bc
Starklord17 <maxi.sm117@gmail.com>
2024-07-20 12:46:13 -0300

    feat: Add getOrderById server action and implement it in OrderIDpage component

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,9 +1,12 @@
-import Link from "next/link";
+// import Link from "next/link";
 import Image from "next/image";
 import { QuantitySelector, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 import clsx from "clsx";
 import { IoCartOutline } from "react-icons/io5";
+import { getOrderById } from "@/actions/order/get-order-by-id";
+import { redirect } from "next/navigation";
+import { currencyFormat } from "@/utils";
 
 const productsInCart = [
   initialData.products[0],
@@ -17,16 +20,27 @@ interface Props {
   };
 }
 
-export default function OrderIDpage({ params }: Props) {
+export default async function OrderIDpage({ params }: Props) {
   const { id } = params;
 
-  // TODO: verificar
-  // redirect(/)
+  // TODO: Llamar el server action getOrderById
+
+  const { ok, order } = await getOrderById(id);
+
+  if (!ok) {
+    redirect("/");
+  }
+
+  // console.log(order);
+  // console.log( JSON.stringify(order) );
+
+  const address = order?.OrderAddress;
 
   return (
     <main className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <section className="flex flex-col w-[1000px]">
-        <Title title={`Orden #${id}`} />
+        {/* <Title title={`Orden #${id}`} /> */}
+        <Title title={`Orden #${id.split("-").at(-1)}`} />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           {/* Carrito */}
@@ -46,24 +60,32 @@ export default function OrderIDpage({ params }: Props) {
             </div>
 
             {/* Items */}
-            {productsInCart.map((product) => (
-              <div key={product.slug} className="flex mb-5">
+            {order!.OrderItem.map((item) => (
+              <div
+                key={item.product.slug + "-" + item.size}
+                className="flex mb-5"
+              >
                 <Image
-                  src={`/products/${product.images[0]}`}
-                  alt={product.title}
+                  src={`/products/${item.product.ProductImage[0].url}`}
+                  alt={item.product.title}
                   width={100}
                   height={100}
                   style={{
                     objectFit: "cover",
+                    width: "auto",
+                    height: "100%",
                   }}
+                  priority
                   className="mr-5 rounded"
                 />
 
                 <div>
-                  <p>{product.title}</p>
-                  <p>${product.price}</p>
+                  <p>{item.product.title}</p>
+                  <p>
+                    ${item.price} x {item.quantity}{" "}
+                  </p>
                   <p className="font-semibold mt-2">
-                    Subtotal: ${product.price * 3}
+                    Subtotal: {currencyFormat(item.price * item.quantity)}
                   </p>
 
                   {/* <div className="mt-2">
@@ -80,12 +102,16 @@ export default function OrderIDpage({ params }: Props) {
           <aside className="bg-white rounded-xl shadow-xl p-7 flex flex-col justify-between">
             <h2 className="text-2xl mb-2">Dirección de entrega</h2>
             <div className="mb-10">
-              <p className="text-xl">Bart Simpson</p>
-              <p>Av. Siempre viva 123</p>
-              <p>Springfield, 12345</p>
-              <p>Estados Unidos</p>
-              <p>Código Postal: 7600</p>
-              <p>Tel: 333444555</p>
+              <p className="text-xl">
+                {address!.firstName} {address!.lastName}
+              </p>
+              <p>{address!.address}</p>
+              <p>{address!.address2}</p>
+              <p>
+                {address!.city}, {address!.countryId}
+              </p>
+              <p>{address!.postalCode}</p>
+              <p>{address!.phone}</p>
             </div>
 
             {/* Divider */}
@@ -95,16 +121,24 @@ export default function OrderIDpage({ params }: Props) {
 
             <div className="grid grid-cols-2 gap-2">
               <span>Nº Productos</span>
-              <span className="text-right">3 artículos</span>
+              <span className="text-right">
+                {order?.itemsInOrder === 1
+                  ? "1 artículo"
+                  : `${order?.itemsInOrder} artículos`}
+              </span>
 
               <span>Subtotal</span>
-              <span className="text-right">$ 100</span>
+              <span className="text-right">
+                {currencyFormat(order!.subTotal)}
+              </span>
 
               <span>Impuestos (15%)</span>
-              <span className="text-right">$ 15</span>
+              <span className="text-right">{currencyFormat(order!.tax)}</span>
 
               <span className="mt-5 text-2xl">Total:</span>
-              <span className="mt-5 text-2xl text-right">$ 115</span>
+              <span className="mt-5 text-2xl text-right">
+                {currencyFormat(order!.total)}
+              </span>
             </div>
 
             <button className="mt-5 mb-2 w-full">
@@ -121,7 +155,6 @@ export default function OrderIDpage({ params }: Props) {
                 {/* <span className="mx-2">Pendiente de pago</span> */}
                 <span className="mx-2">Pagada</span>
               </div>
-
             </button>
           </aside>
         </div>
