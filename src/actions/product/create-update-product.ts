b38086b7a3d2f26748de4723a3fd17d9d34d09b1
Starklord17 <maxi.sm117@gmail.com>
2024-07-23 15:00:22 -0300

    Actualizar producto

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -1,6 +1,7 @@
 'use server';
 
-import { Gender } from '@prisma/client';
+import prisma from '@/lib/prisma';
+import { Gender, Product, Size } from '@prisma/client';
 import {z} from 'zod';
 
 // Esquema de validación de los datos del producto. Sirve para no tener que hacer validaciones manuales con regex.
@@ -36,7 +37,44 @@ export const createUpdateProduct = async (formData: FormData) => {
     }
   }
 
-  console.log(productParsed.data);
+  // console.log(productParsed.data);
+  const product = productParsed.data;
+  product.slug = product.slug.toLowerCase().replace(/ /g, '-').trim(); // El modificador /g se utiliza para indicar que la operación de reemplazo debe aplicarse de manera global a toda la cadena de texto.
+
+  const {id, ...rest} = product;
+
+  const prismaTx = await prisma.$transaction(async (tx) => {
+
+    let product: Product;
+    const tagsArray = rest.tags.split(',').map( tag => tag.trim().toLowerCase() );
+
+    if (id) {
+      // Actualizar
+      product = await prisma.product.update({
+        where: {id},
+        data: {
+          ...rest,
+          sizes: {
+            set: rest.sizes as Size[],
+          },
+          tags: {
+            set: tagsArray,
+          }
+        }
+      });
+
+      console.log({updatedProduct: product});
+
+    } else {
+      // Crear
+    }
+
+    return {
+
+    }
+  });
+
+  // TODO: RevalidatePaths
 
   return {
     ok: true
