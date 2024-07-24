d0dddc5d1c20105d6eebf617b933f402b340ab23
Starklord17 <maxi.sm117@gmail.com>
2024-07-23 23:23:04 -0300

    refactor: Update exports in actions/index.ts
    Clean up and organize the exports in actions/index.ts file to improve code readability and maintainability.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,27 +1,47 @@
-export * from "./product/product-pagination";
-export * from "./product/get-product-by-slug";
-export * from "./product/get-stock-by-slug";
-export * from "./product/create-update-product";
+// export * from "./product/product-pagination";
+// export * from "./product/get-product-by-slug";
+// export * from "./product/get-stock-by-slug";
+// export * from "./product/create-update-product";
 
-export * from "./auth/login";
-export * from "./auth/logout";
-export * from "./auth/register";
+export { getPaginatedProductsWithImages } from "./product/product-pagination";
+export { getProductBySlug } from "./product/get-product-by-slug";
+export { getStockBySlug } from "./product/get-stock-by-slug";
+export { createUpdateProduct } from "./product/create-update-product";
 
-export * from "./country/get-countries";
+// export * from "./auth/login";
+export {authenticate, login} from "./auth/login";
+// export * from "./auth/logout";
+export {logout} from "./auth/logout";
+// export * from "./auth/register";
+export {registerUser} from "./auth/register";
 
-export * from "./address/set-user-address";
-export * from "./address/delete-user-address";
-export * from "./address/get-user-address";
+// export * from "./country/get-countries";
+export {getCountries} from "./country/get-countries";
 
-export * from "./order/place-order";
-export * from "./order/get-orders-by-user";
-export * from "./order/get-order-by-id";
-export * from "./order/get-paginated-orders";
+// export * from "./address/set-user-address";
+export {setUserAddress} from "./address/set-user-address";
+// export * from "./address/delete-user-address";
+export {deleteUserAddress} from "./address/delete-user-address";
+// export * from "./address/get-user-address";
+export {getUserAddress} from "./address/get-user-address";
 
-export * from "./payments/set-transaction-id";
-export * from "./payments/paypal-check-payment";
+// export * from "./order/place-order";
+export {placeOrder} from "./order/place-order";
+// export * from "./order/get-orders-by-user";
+export {getOrdersByUser} from "./order/get-orders-by-user";
+// export * from "./order/get-order-by-id";
+export {getOrderById} from "./order/get-order-by-id";
+// export * from "./order/get-paginated-orders";
+export {getPaginatedOrders} from "./order/get-paginated-orders";
 
-export * from "./user/get-paginated-users";
-export * from "./user/change-user-role";
+// export * from "./payments/set-transaction-id";
+export {setTransactionId} from "./payments/set-transaction-id";
+// export * from "./payments/paypal-check-payment";
+export {paypalCheckPayment} from "./payments/paypal-check-payment";
 
-export * from "./category/get-categories";
+// export * from "./user/get-paginated-users";
+export {getPaginatedUsers} from "./user/get-paginated-users";
+// export * from "./user/change-user-role";
+export {changeUserRole} from "./user/change-user-role";
+
+export {getCategories} from "./category/get-categories";
